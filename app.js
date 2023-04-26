4fa5d86e941ca7090cdfd1bc60ae8595a0774298
sebbex1337 <sefort@hotmail.dk>
2023-04-26 12:00:34 +0200

    Changed logic of displayUsers and updateUsersGrid

--- app.js
+++ app.js
@@ -3,13 +3,25 @@
 window.addEventListener("load", initApp);
 
 const ENDPOINT = "https://crud-app-group-project-default-rtdb.europe-west1.firebasedatabase.app/";
+let users;
 
-async function initApp() {
-	const users = await getUsers(`${ENDPOINT}/users.json`);
-	for (const user of users) {
-		displayUser(user);
-	}
-	/* createUser("https://images.unsplash.com/photo-1642049888276-9c9f0a1a8758?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyOTA4MTB8MHwxfGFsbHwyfHx8fHx8Mnx8MTY0MjA3NTAwMQ&ixlib=rb-1.2.1&q=80&w=400", "Test", 99); */
+function initApp() {
+	updateUsersGrid();
+
+	document.querySelector("#createUser").addEventListener("click", createUserClicked);
+}
+
+/* Event functions */
+
+function createUserClicked() {
+	document.querySelector("#dialog-create-user").showModal();
+}
+
+/* Get users functions */
+
+async function updateUsersGrid() {
+	users = await getUsers(`${ENDPOINT}/users.json`);
+	displayUsers(users);
 }
 
 async function getUsers(url) {
@@ -29,19 +41,21 @@ function prepareUsersData(dataObject) {
 	return newData;
 }
 
-async function createUser(image, name, age) {
-	const newUser = { image, name, age };
+/* CRUD functions */
+
+async function createUser(image, name, age, gender, role, mail, team, city, meal, fun_fact, operatingSystem, pineapple) {
+	const newUser = { image, name, age, gender, role, mail, team, city, meal, fun_fact, operatingSystem, pineapple };
 	const userAsJson = JSON.stringify(newUser);
 	const response = await fetch(`${ENDPOINT}/users.json`, {
 		method: "POST",
 		body: userAsJson,
 	});
 	const data = await response.json();
-	console.log(data);
+	console.log("User created " + data);
 }
 
-async function updateUser(id, image, name, age) {
-	const userToUpdate = { image, name, age };
+async function updateUser(id, image, name, age, gender, role, mail, team, city, meal, fun_fact, operatingSystem, pineapple) {
+	const userToUpdate = { image, name, age, gender, role, mail, team, city, meal, fun_fact, operatingSystem, pineapple };
 	const userAsJson = JSON.stringify(userToUpdate);
 	const response = await fetch(`${ENDPOINT}/users/${id}.json`, {
 		method: "PUT",
@@ -51,15 +65,28 @@ async function updateUser(id, image, name, age) {
 	console.log(data);
 }
 
-function displayUser(user) {
-	document.querySelector("#users").insertAdjacentHTML(
-		"beforeend",
-		/*html*/ `
+async function deleteUser(id) {
+	const response = await fetch(`${ENDPOINT}/users/${id}.json`, { method: "DELETE" });
+	if (response.ok) {
+		updateUsersGrid();
+	}
+}
+
+/* UI functions */
+
+function displayUsers(users) {
+	for (const user of users) {
+		document.querySelector("#users").insertAdjacentHTML(
+			"beforeend",
+			/*html*/ `
 		<article class="grid-item">
 			<img src="${user.image}">
 			<h2>${user.name}</h2>
 			<p>Age: ${user.age}</p>
+			<p>Mail: ${user.mail}</p>
+			<p>Team: ${user.team}</p>
 		</article>
 	`
-	);
+		);
+	}
 }
