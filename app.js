e8cfdbbdd1b61cbf3f59c75a02719a6a2a6cb09a
sebbex1337 <sefort@hotmail.dk>
2023-04-28 18:51:32 +0200

    Added functionality to create user

--- app.js
+++ app.js
@@ -15,10 +15,27 @@ function initApp() {
 
 function showCreateUserDialog() {
 	document.querySelector("#dialog-create-user").showModal();
+	document.querySelector("#form-create-new-user").addEventListener("submit", createUserClicked);
 }
 
 function createUserClicked(event) {
 	event.preventDefault();
+	const form = this;
+	const name = form.name.value;
+	const image = form.image.value;
+	const age = form.age.value;
+	const gender = form.gender.value;
+	const role = form.role_select.value;
+	const mail = form.mail.value;
+	const team = form.team_select.value;
+	const city = form.city.value;
+	const meal = form.meal.value;
+	const fun_fact = form.fun_fact.value;
+	const operatingSystem = form.operatingSystem.value;
+	const pineapple = form.pineapple.value;
+	createUser(image, name, age, gender, role, mail, team, city, meal, fun_fact, operatingSystem, pineapple);
+	form.reset();
+	document.querySelector("#dialog-create-user").close();
 }
 
 /* Get users functions */
@@ -80,19 +97,40 @@ async function deleteUser(id) {
 
 /* UI functions */
 
-function displayUsers(users) {
-	for (const user of users) {
-		document.querySelector("#users").insertAdjacentHTML(
-			"beforeend",
-			/*html*/ `
+function displayUsers(listOfUsers) {
+	document.querySelector("#users").innerHTML = ""; // Reset users list in html
+
+	for (const user of listOfUsers) {
+		displayUser(user);
+	}
+}
+
+function displayUser(user) {
+	document.querySelector("#users").insertAdjacentHTML(
+		"beforeend",
+		/*html*/ `
 		<article class="grid-item">
 			<img src="${user.image}">
 			<h2>${user.name}</h2>
 			<p>Age: ${user.age}</p>
 			<p>Mail: ${user.mail}</p>
 			<p>Team: ${user.team}</p>
+			<section class="btns">
+				<button class="btn-delete">Delete</button>
+				<button class="btn-update">Update</button>
+			</section>
 		</article>
 	`
-		);
+	);
+
+	document.querySelector("#users article:last-child .btn-delete").addEventListener("click", deleteClicked);
+	document.querySelector("#users article:last-child .btn-update").addEventListener("click", updateClicked);
+
+	function deleteClicked() {
+		console.log("Delete button clicked");
+	}
+
+	function updateClicked() {
+		console.log("Update button clicked");
 	}
 }
